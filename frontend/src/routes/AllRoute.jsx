d6726922369cf38780c54f95b10578f9755bd013
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-06-15 23:49:51 +0530

    Footer and Navbar created

--- frontend/src/routes/AllRoute.jsx
+++ frontend/src/routes/AllRoute.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Courses from '../components/Adminitems/Courses'
-import DashBoard from '../components/Adminitems/DashBoard'
-import EditPage from '../components/Adminitems/EditPage'
-import AddCourse from '../components/Adminitems/AddCourse'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Courses from "../components/Adminitems/Courses";
+import DashBoard from "../components/Adminitems/DashBoard";
+import EditPage from "../components/Adminitems/EditPage";
+import AddCourse from "../components/Adminitems/AddCourse";
+import LandingPage from "../Pages/LandingPage";
 
 const AllRoute = () => {
   return (
     <Routes>
-      <Route path='/admin' element={<DashBoard/>} />
-      <Route path='/admin/dashboard' element={<DashBoard/>} />
-      <Route path="/admin/courses" element={<Courses/>} />
-      <Route path="/admin/edit/:id" element={<EditPage/>} />
-      <Route path='/admin/addCourse' element={<AddCourse/>}/>
+      <Route path="/admin" element={<DashBoard />} />
+      <Route path="/admin/dashboard" element={<DashBoard />} />
+      <Route path="/admin/courses" element={<Courses />} />
+      <Route path="/admin/edit/:id" element={<EditPage />} />
+      <Route path="/admin/addCourse" element={<AddCourse />} />
+      <Route path="/" element={<LandingPage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoute
+export default AllRoute;
