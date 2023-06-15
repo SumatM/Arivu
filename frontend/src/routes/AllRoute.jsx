cca0002e7c934d4542830287d762f60c097d3e85
Arun Saradgi <saradgi07@gmail.com>
2023-06-15 23:09:38 +0530

    GAuth

--- frontend/src/routes/AllRoute.jsx
+++ frontend/src/routes/AllRoute.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Courses from '../components/items/Courses'
 import DashBoard from '../components/items/DashBoard'
 import EditPage from '../components/items/EditPage'
+import GoogleAuth from '../pages/GoogleAuth'
 
 const AllRoute = () => {
   return (
@@ -10,6 +11,7 @@ const AllRoute = () => {
       <Route path="/courses" element={<Courses/>} />
       <Route path="/" element={<DashBoard/>} />
       <Route path="/edit" element={<EditPage/>} />
+      <Route path="/googleauth" element={<GoogleAuth/>} />
     </Routes>
   )
 }
