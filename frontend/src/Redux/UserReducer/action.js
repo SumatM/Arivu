9f277ec4875ecbcd97ecd6c5a9e9caa8d25ef4c8
Arun Saradgi <saradgi07@gmail.com>
2023-06-19 14:26:38 +0530

    dropandUserProfile

--- frontend/src/Redux/UserReducer/action.js
+++ frontend/src/Redux/UserReducer/action.js
@@ -19,7 +19,14 @@ export const loginFetch = (value) => (dispatch) => {
       dispatch(actionLoginSuccess(res.data));
       localStorage.setItem(
         "user",
-        JSON.stringify({email: res.data.user.email,name: res.data.user.name,role:res.data.user.role,token: res.data.token,isAuth: true})
+        JSON.stringify({
+          email: res.data.user.email,
+          name: res.data.user.name,
+          role: res.data.user.role,
+          userId:res.data.user._id,
+          token: res.data.token,
+          isAuth: true,
+        })
       );
       console.log(res);
     })
@@ -29,25 +36,26 @@ export const loginFetch = (value) => (dispatch) => {
     });
 };
 
-
 export const signUpFetch = (value) => (dispatch) => {
-   dispatch(actionsignUpLoading())
-  return  axios.post(`${baseURL}users/register`,value)
-    .then((res)=>{
-    dispatch(actionsingUpSuccess())
-        console.log(res);
+  dispatch(actionsignUpLoading());
+  return axios
+    .post(`${baseURL}users/register`, value)
+    .then((res) => {
+      dispatch(actionsingUpSuccess());
+      console.log(res);
     })
-    .catch((err)=>{
-    dispatch(actionsingUpError(err.response.data.msg))
-        console.log(err.response.data.msg);
-    })
-}
+    .catch((err) => {
+      dispatch(actionsingUpError(err.response.data.msg));
+      console.log(err.response.data.msg);
+    });
+};
 
-
-// conver 1 letter to upper case and rest to lower 
+// conver 1 letter to upper case and rest to lower
 export function capitalizeFirstLetter(string) {
   console.log(string);
-  const words = string?.split(' ');
-  const capitalizedWords = words?.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-  return capitalizedWords?.join(' ');
+  const words = string?.split(" ");
+  const capitalizedWords = words?.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  );
+  return capitalizedWords?.join(" ");
 }
